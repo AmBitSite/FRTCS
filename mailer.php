87fdfbd0e59a55e8bd553a79bdba2efdc1d6a33d
AmBitSite <yfljvtymitgbnmrtif@yandex.ru>
2020-03-03 10:24:22 +0300

    03.03.2020

--- mailer.php
+++ mailer.php
@@ -1,18 +1,10 @@
 <?php
-//// ФАЙЛ ДЛЯ РАССЫЛКИ
-////Получаем данные из формы
-
-$name = $_POST['name']; // поле с Заговоком
-$email = $_POST['email']; // поле с Текстом сообщения
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-  echo 'email is not valid ';
-};
+$name = $_POST['name'];
+$email = $_POST['email'];
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){echo 'email is not valid ';};
 $title = 'First Trust and Consulting services - Site Message ';
 $message = "Name: $name \n\r
             Email: $email ";
-
-mail('info@ftcs.com', $title, $message);
-
+mail('info@firsttacs.com', $title, $message);
 header("Location: /index.html");
 exit();
